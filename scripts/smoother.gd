b93d8c6683cb96a99f71819d547e021ad4a05ffe
Daniel Bernal <danisebber1234@gmail.com>
2024-11-17 23:24:17 -0400

    smoother fix

--- scripts/smoother.gd
+++ scripts/smoother.gd
@@ -18,8 +18,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-class_name Smoother
+
 extends Node
+class_name Smoother
 ## Smoother Node
 ## Version: 1.0.5
 ##
